cd85d9a9dc4bfac464680a6acf114cca06aebb4e
SandeepKumar <sandeepkusolanki@gamil.com>
2025-03-31 02:01:23 +0530

    hooks

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,23 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+
+  const [buttonClick , setButtonClick] = useState(false);
+
+  function handleClick(){
+    setButtonClick(true);
+  }
+
   return (
     <div id="main">
-      // Do not alter the main div
+      {/* // Do not alter the main div */}
+      <button 
+        id="click"
+        onClick={handleClick}
+      >Click Me</button>
+      {buttonClick && (
+        <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p>
+      )}
     </div>
   );
 }
